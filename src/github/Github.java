6e073211d8e7720e476f576c6023834691ff5a06
hamjew <hamjew@sheridancollege.ca>
2020-01-29 16:52:11 -0500

    adding

--- src/github/Github.java
+++ src/github/Github.java
@@ -4,8 +4,9 @@ public class Github {
 
     public static void main(String[] args) {
         
+        System.out.println("Github Test");
         
-        
+        System.out.println("Checking some changes");
     }
     
 }
